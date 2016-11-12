02eef12f8fdfa15f48d1832f36b8f14243299af7
Francesc Riart Pola <cesc.riart@student.anglia.ac.uk>
2016-11-12 12:32:36 +0000

    Adds Errror Checker Class

---
+++ CVFeedbackApp/CVFeedbackApp/ErrorChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CVFeedbackApp
+{
+    
+    class ErrorChecker
+    {
+        static private string errorCaption = "Error detected";
+        static public bool nullTextboxErrorCheck (string textboxName , string textboxContent)
+        {
+            if (textboxContent == "")
+            {
+                string message = "You did not enter any text in " + textboxName;
+                string caption = errorCaption;
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                // Displays the MessageBox.
+
+                result = MessageBox.Show(message, caption, buttons);
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
